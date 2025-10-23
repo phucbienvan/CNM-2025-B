f9afe92e7a4a611a3b1da53411c137fd17fe21ab
Le Dai Thanh <thanh261220@gmail.com>
2025-10-23 15:16:36 +0700

    DeleteTask

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -28,4 +28,18 @@ class TaskController extends Controller
 
         return TaskResource::apiPaginate($tasks, $request);
     }
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        if(!$task){
+            return response()->json([
+                'message' => 'Task not found'
+            ],404);
+        }
+
+        $task->delete();
+        return response()->json([
+            'message' => 'Task deleted successfully'
+        ],200);
+    }
 }

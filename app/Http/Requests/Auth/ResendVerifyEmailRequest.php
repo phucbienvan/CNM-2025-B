ec676567e98efffb2783ec07668c9cfc74465546
Phạm Thiện Tâm <22115053122336@sv.ute.udn.vn>
2025-11-18 23:44:50 +0700

    add resend email

---
+++ app/Http/Requests/Auth/ResendVerifyEmailRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+use App\Http\Requests\BaseRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ResendVerifyEmailRequest extends BaseRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+             'email' => 'required|string|email|max:255',
+        ];
+    }
+}
